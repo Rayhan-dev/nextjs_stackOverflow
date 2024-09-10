6dce752c428bb8a37c65b69ea5d254aaa7a13d31
Mohammod Rayhan <mohammodrayhan444@gmail.com>
2024-09-11 02:08:02 +0600

    added metadata,fonts,favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,45 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrowtesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrowtesk",
+});
+
+export const metadata = {
+  title: "devflow",
+  description: "Stack overflow clone and more",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrowtesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
