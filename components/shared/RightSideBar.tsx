13851c49113f681ca3d1c2e1b45c9d2cdb10b218
Mohammod Rayhan <mohammodrayhan444@gmail.com>
2024-09-19 01:53:43 +0600

    homepage completed with dummy data

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -6,38 +6,38 @@ import RenderTag from "./RenderTag";
 const RightSideBar = () => {
   const hotQuestions = [
     {
-      _id: 1,
+      _id: "1",
       titel:
         "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
     },
-    { _id: 2, titel: "Is it only me or the font is bolder than necessary?" },
-    { _id: 3, titel: "Redux Toolkit Not Updating State as Expected" },
-    { _id: 4, titel: "Async/Await Function Not Handling Errors Properly" },
-    { _id: 5, titel: "Can I get the course for free?" },
+    { _id: "2", titel: "Is it only me or the font is bolder than necessary?" },
+    { _id: "3", titel: "Redux Toolkit Not Updating State as Expected" },
+    { _id: "4", titel: "Async/Await Function Not Handling Errors Properly" },
+    { _id: "5", titel: "Can I get the course for free?" },
   ];
   const popularTags = [
     {
-      _id: 1,
+      _id: "1",
       name: "NEXTJS",
       totalQuestions: 32,
     },
     {
-      _id: 2,
+      _id: "2",
       name: "TEST",
       totalQuestions: 9,
     },
     {
-      _id: 3,
+      _id: "3",
       name: "REACT",
       totalQuestions: 18,
     },
     {
-      _id: 4,
+      _id: "4",
       name: "CSS",
       totalQuestions: 13,
     },
     {
-      _id: 5,
+      _id: "5",
       name: "REDUX",
       totalQuestions: 5,
     },
--- components/shared/cards/QuestionCard.tsx
+++ components/shared/cards/QuestionCard.tsx
@@ -1,4 +1,8 @@
+import Link from "next/link";
 import React from "react";
+import RenderTag from "../RenderTag";
+import Metric from "../Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
 interface QuestionProps {
   _id: string;
@@ -29,8 +33,61 @@ const QuestionCard = ({
 }: QuestionProps) => {
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-      <div></div>
-      {title}
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row ">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimestamp(createdAt)}
+          </span>
+          <Link href={`/questions/${_id}`}>
+            <h3 className="base-bold sm:h3-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+      </div>
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <RenderTag
+            key={tag._id}
+            _id={tag._id}
+            name={tag.name}
+            totalQuestion={0}
+            showCount={false}
+          />
+        ))}
+      </div>
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl="/assets/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title="- asked 1 hour ago"
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="upvotes"
+          value={formatAndDivideNumber(upvotes)}
+          title="Votes"
+          textStyles="small-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="answers"
+          value={formatAndDivideNumber(answers.length)}
+          title="Answers"
+          textStyles="small-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="views"
+          value={formatAndDivideNumber(views)}
+          title="Views"
+          textStyles="small-medium text-dark400_light700"
+        />
+      </div>
     </div>
   );
 };
