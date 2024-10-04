75ed2cde0903845957f6f98fc9a3f199ef7c8fbb
Mohammod Rayhan <mohammodrayhan444@gmail.com>
2024-10-04 17:43:35 +0600

    added users and question actions

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+
+export async function getUserById(userId: string) {
+  try {
+    connectToDatabase();
+
+    const user = await User.findOne({ clerkId: userId });
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
---
+++ lib/mongoose.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) {
+    console.log("Missing Mongodb URL");
+    return;
+  }
+  if (isConnected) {
+    console.log("Mongodb already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "devflow",
+    });
+    isConnected = true;
+    console.log("Mongodb connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
