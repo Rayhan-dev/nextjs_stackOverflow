384ec62e45857a655633f3c08d168afa1dbdbcb9
Mohammod Rayhan <mohammodrayhan444@gmail.com>
2024-10-20 19:29:16 +0600

    add profile edit funtionality

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -35,7 +35,9 @@ const Question = ({ type, questionDetails, mongoUserId }: Props) => {
   const router = useRouter();
   const pathname = usePathname();
   const parsedQuestionDetails = JSON.parse(questionDetails || "{}");
-  const groupedTags = parsedQuestionDetails.tags.map((tag) => tag.name);
+  const groupedTags = parsedQuestionDetails.tags.map(
+    (tag: { name: any }) => tag.name
+  );
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
